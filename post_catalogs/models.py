7fbb835d76b6fba6927811f55d40ed7bdfa91964
Wagura Maurice <business@waguramaurice.com>
2024-03-17 20:15:29 +0300

    update models.py

--- post_catalogs/models.py
+++ post_catalogs/models.py
@@ -7,12 +7,12 @@ from post_categories.models import Category
 User = get_user_model()
 
 class Catalog(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='catalogs')
     title = models.CharField(max_length=255)
     content = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_catalogs')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_catalogs')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='catalogs')
 
     def __str__(self):
         return self.title
