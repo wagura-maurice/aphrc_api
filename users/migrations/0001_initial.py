007f5a5fcd8d9151207e71575b7636cec5045cd4
Wagura Maurice <business@waguramaurice.com>
2024-03-17 18:58:55 +0300

    update 0001_initial.py

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -21,6 +21,7 @@ class Migration(migrations.Migration):
                 ('email', models.EmailField(max_length=255, unique=True)),
                 ('is_active', models.BooleanField(default=True)),
                 ('is_staff', models.BooleanField(default=False)),
+                ('is_superuser', models.BooleanField(default=False)),
             ],
             options={
                 'abstract': False,
