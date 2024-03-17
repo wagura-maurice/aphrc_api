676b946202f2d1ca207644e5d32972db6fb0cff7
Wagura Maurice <business@waguramaurice.com>
2024-03-17 20:15:36 +0300

    update models.py

--- post_categories/models.py
+++ post_categories/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     class Meta:
         verbose_name_plural = 'Post Categories'
 
