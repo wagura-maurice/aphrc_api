48e7f9664122b0da5b422cd04f3cf228079a289a
Wagura Maurice <business@waguramaurice.com>
2024-03-18 03:29:23 +0300

    update apps.py

--- management/apps.py
+++ management/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
+# The `ManagementConfig` class in this Python code snippet sets the default auto field to
+# `django.db.models.BigAutoField` for the `management` app.
 class ManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'management'
